fbf8d292320e21050ff66266c1d757c91d5354c5
Rathiesh Prasad <rathiesh2002@gmail.com>
2024-02-14 16:02:55 +0530

    next commit

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 import math
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField,StreamField
 from wagtail.admin.panels import FieldPanel
+from wagtail.blocks import ChoiceBlock
 from wagtail.search import index
 
 class BlogIndexPage(Page):
@@ -32,11 +33,28 @@ class BlogPage(Page):
         FieldPanel('body'),
         FieldPanel('image'),
         FieldPanel('caption'),
+        FieldPanel('category'),
     ]    
 
+    category = StreamField([
+        ('category', ChoiceBlock(choices=[
+            ('HappyFox Help Desk', 'HappyFox Help Desk'),
+            ('HappyFox Service Desk', 'HappyFox Service Desk'),
+            ('HappyFox BI', 'HappyFox BI'),
+            ('HappyFox ChatBot', 'HappyFox ChatBot'),
+            ('Miscellaneous', 'Miscellaneous'),
+        ])),
+    ], blank=True)
+    def get_related_posts(self):
+        category = self.category 
+        parent_page = self.get_parent().specific
+        related_posts = parent_page.get_children().type(BlogPage).filter(category=category).exclude(id=self.id)[:3]
+        return related_posts
     def get_reading_time(self):
         words_per_minute = 200
         total_words = len(self.body.split())
         reading_time_minutes = total_words / words_per_minute
         reading_time_minutes = math.ceil(reading_time_minutes)  
         return reading_time_minutes
+    
+    
